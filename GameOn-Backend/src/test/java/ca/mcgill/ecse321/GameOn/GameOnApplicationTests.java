5ccf2fb2776c5bbe994b5c1d9d1c7c533b648abc
Neeshal-Imrit <116143878+Neeshal-Imrit@users.noreply.github.com>
2024-09-29 20:09:25 -0400

    Spring Boot init

---
+++ GameOn-Backend/src/test/java/ca/mcgill/ecse321/GameOn/GameOnApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.GameOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
