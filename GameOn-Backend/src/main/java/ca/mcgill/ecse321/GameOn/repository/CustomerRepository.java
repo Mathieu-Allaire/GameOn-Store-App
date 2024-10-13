115402db6c597a34e00fe0e6efd83dd52c63c805
JRfeghaly <josephfeghaly2004@gmail.com>
2024-10-12 20:15:25 -0400

    fixed some stuff could some1 test it?

--- GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/repository/CustomerRepository.java
+++ GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/repository/CustomerRepository.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse321.GameOn.repository
+package ca.mcgill.ecse321.GameOn.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.GameOn.model.Customer;
