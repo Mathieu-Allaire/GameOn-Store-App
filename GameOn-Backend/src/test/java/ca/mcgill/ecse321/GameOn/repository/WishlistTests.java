018db09e94744fb11e17c959b496628d70b96c8d
Luis Felipe Jarquin Romero <luis.jarquinromero@mail.mcgill.ca>
2024-10-14 18:23:19 -0400

    add wishlistTests.java

---
+++ GameOn-Backend/src/test/java/ca/mcgill/ecse321/GameOn/repository/WishlistTests.java
@@ -0,0 +1,36 @@
+package ca.mcgill.ecse321.GameOn.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ca.mcgill.ecse321.GameOn.model.Wishlist;
+
+@SpringBootTest
+public class WishlistTests {
+    
+    @Autowired
+    private WishlistRepository wishlistRepo;
+    
+    @BeforeEach
+    @AfterEach
+    public void clearDatabase() {
+        wishlistRepo.deleteAll();
+    }
+
+    @Test
+    public void testCreateAndReadWishlist(){
+        Wishlist wishlist = new Wishlist();
+        wishlist = wishlistRepo.save(wishlist);
+        int id = wishlist.getId();
+        Wishlist wishlist_from_DB = wishlistRepo.findWishlistById(id);
+        assertNotNull(wishlist_from_DB);
+        assertEquals(wishlist_from_DB.getId(),id);  
+    }
+     
+
+}
