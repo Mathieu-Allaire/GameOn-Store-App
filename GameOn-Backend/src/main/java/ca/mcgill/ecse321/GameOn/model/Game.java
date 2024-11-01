569bace67cde455299d5a1be642c064d40d7c045
Luis Felipe Jarquin Romero <luis.jarquinromero@mail.mcgill.ca>
2024-11-01 17:05:58 -0400

    private boolean isInStock

--- GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/model/Game.java
+++ GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/model/Game.java
@@ -27,6 +27,8 @@ public class Game
   private int price;
   private int quantity;
 
+  private boolean isInStock;
+
   //Game State Machines
   public enum GameStatus { Available, OutOfStock, Unavailable }
   private GameStatus gameStatus;
