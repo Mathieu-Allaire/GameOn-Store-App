b12d5a58b75eda94d09f12fa1e6c7bac471c87e8
Mathieu <mathieu.allaire2@mail.mcgill.ca>
2024-11-06 11:02:16 -0500

    Finished Review Service

--- GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/service/reviewService.java
+++ GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/service/reviewService.java
@@ -12,9 +12,6 @@ import ca.mcgill.ecse321.GameOn.exception.reviewException;
 import ca.mcgill.ecse321.GameOn.repository.ReviewRepository;
 import ca.mcgill.ecse321.GameOn.model.Review;
 
-//import ca.mcgill.ecse321.eventregistration.model.Event;
-//import ca.mcgill.ecse321.eventregistration.model.Person;
-
 
 @Service
 public class ReviewService {
@@ -22,60 +19,91 @@ public class ReviewService {
     private ReviewRepository reviewRepo;
 
 
+    /**
+     * Posts a new review with the specified details and saves it to the repository.
+     * Validates input parameters to ensure the review is properly structured.
+     * @author Mathieu Allaire
+     * @param aDescription A description of the review.
+     * @param aStars The rating given in the review, between 0 and 5 stars.
+     * @param aLikes The initial number of likes for the review, must be non-negative.
+     * @param aDislikes The initial number of dislikes for the review, must be non-negative.
+     * @param aReviewAuthor The customer who authored the review.
+     * @param aManager The manager responsible for approving or managing the review.
+     * @return The newly created review object.
+     * @throws IllegalArgumentException if any parameters are invalid.
+     */
+    public Review postReview(String aDescription, int aStars, int aLikes, int aDislikes, Customer aReviewAuthor, Manager aManager){
+        if(aDescription == null || aDescription.length() == 0){
+            IllegalArgumentException("The review has an empty description");
+        }
+        if(aStars < 0 || aStars > 5){
+            throw new IllegalArgumentException("The number of stars must be between 0 and 5");
+        }
+        if(aLikes < 0){
+            throw new IllegalArgumentException("The number of likes must be non-negative");
+        }
+        if(aDislikes < 0){
+            throw new IllegalArgumentException("The number of dislikes must be non-negative");
+        }
+        if(aReviewAuthor == null){
+            throw new IllegalArgumentException("The author is invalid");
+        }
+        if(aManager == null){
+            throw new IllegalArgumentException("The manager is invalid");
+        }
+        Review review = new Review(aDescription, aStars, aLikes, aDislikes, aReviewAuthor, aManager);
+
+        reviewRepo.save(review);
+        return review;
+    }
+
+    /**
+     * Finds a review by its ID in the repository.
+     * @author Mathieu Allaire
+     * @param id The ID of the review to find.
+     * @return The review with the specified ID.
+     * @throws IllegalArgumentException if no review with the given ID exists.
+     */
     public Review findReviewById(int id){
         Review existingReview = reviewRepo.findReviewById(int id);
         if(existingReview == null){
             throw new IllegalArgumentException("There is no review with ID " + id + ".");
         }
         return existingReview;
-
     }
 
-    public void likeReview(){}
-    public void postReview(){}
-    public int getStars(){}
-    public void addReply(){}
-
-
+    /**
+     * Increments the like count of a given review and saves the updated review.
+     * @author Mathieu Allaire
+     * @param review The review to be liked.
+     * @throws IllegalArgumentException if the review does not exist.
+     */
     @Transactional
-    public Person createPerson(String name, String email, String password) {
-        if (password == null || password.length() < 8) {
-            throw new IllegalArgumentException("Password too short.");
+    public void likeReview(Review review){
+        Review review = findReviewById(review.getId());
+        if(review == null){
+            throw new IllegalArgumentException("There is no review with ID " + id + ".");
         }
-
-        Date now = Date.valueOf(LocalDate.now());
-        Person personToCreate = new Person(name, email, password, now);
-        return personRepo.save(personToCreate);
+        review.setLikes(review.getLikes()+1);
+        reviewRepo.save(review);
     }
 
-    public Registration register(Person person, Event event) {
-        Registration existingRegistration = findRegistration(person, event);
-        if (existingRegistration != null) {
-            // Already registered
-            return existingRegistration;
-        } else {
-            Registration newRegistration = new Registration(new Registration.Key(person, event));
-            return registrationRepo.save(newRegistration);
-        }
-    }
-
-
+    /**
+     * Adds a reply to a review and saves the updated review in the repository.
+     * @author Mathieu Allaire
+     * @param review The review to which the reply is being added.
+     * @param description The description of the reply to be added.
+     * @throws IllegalArgumentException if the reply description is empty or if the review does not exist.
+     */
     @Transactional
-    public void unregister(Person person, Event event) {
-        Registration existingRegistration = findRegistration(person, event);
-        registrationRepo.delete(existingRegistration);
-    }
-
-    public Registration findRegistration(Person person, Event event) {
-        Registration reg = registrationRepo.findRegistrationByKey(new Registration.Key(person, event));
-        if (reg == null) {
-            throw new EventRegistrationException(HttpStatus.NOT_FOUND,
-                    String.format("Person %d is not registered for event %d.", person.getId(), event.getId()));
+    public void addReply(Review review, String description){
+        if(description == null || description.length() == 0){
+            IllegalArgumentException("The reply has an empty description");
         }
-        return reg;
+        Review review = findReviewById(review.getId());
+        if(review == null){
+            throw new IllegalArgumentException("There is no review with ID " + id + ".");
+        }
+        review.setReply(description);
+        reviewRepo.save(review);
     }
-
-    public int countRegistrantsForEvent(Event event) {
-        return registrationRepo.countRegistrationsByKeyEvent(event);
-    }
-}
