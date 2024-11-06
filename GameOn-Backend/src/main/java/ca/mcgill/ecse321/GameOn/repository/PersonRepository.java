4f08a79e019a5918782628f55fde772509ee2458
Luis Felipe Jarquin Romero <luis.jarquinromero@mail.mcgill.ca>
2024-11-06 11:20:21 -0500

    change person repo

--- GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/repository/PersonRepository.java
+++ GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/repository/PersonRepository.java
@@ -3,6 +3,6 @@ package ca.mcgill.ecse321.GameOn.repository;
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.GameOn.model.Person;
 
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-    Person findPersonById(int id);
+public interface PersonRepository extends CrudRepository<Person, String> {
+    Person findPersonByEmail(String email);
 }
