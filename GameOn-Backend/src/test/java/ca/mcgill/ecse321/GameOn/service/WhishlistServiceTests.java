06121a7bb0f56cfb4caabd1698449b5b0916f343
CamiloB6 <camibes06@hotmail.com>
2024-11-08 22:32:53 -0500

    Modified the removeCustomerWish in the Customer class, added test to remove Game, and modified the service function removeGameFromWishlist

--- GameOn-Backend/src/test/java/ca/mcgill/ecse321/GameOn/service/WhishlistServiceTests.java
+++ GameOn-Backend/src/test/java/ca/mcgill/ecse321/GameOn/service/WhishlistServiceTests.java
@@ -23,6 +23,7 @@ import org.springframework.http.HttpStatus;
 
 import ca.mcgill.ecse321.GameOn.model.Cart;
 import ca.mcgill.ecse321.GameOn.model.Customer;
+import ca.mcgill.ecse321.GameOn.model.Employee;
 import ca.mcgill.ecse321.GameOn.model.Game;
 import ca.mcgill.ecse321.GameOn.model.Person;
 import ca.mcgill.ecse321.GameOn.repository.CartRepository;
@@ -108,5 +109,96 @@ public class WhishlistServiceTests {
         assertEquals(customerRole.getCustomerWish().size(), 1);
         
     }
+
+    public void testInvalidCustomerAddGame(){
+        //Create a customer
+        Cart cart = new Cart();
+        Employee employeeRole = new Employee(true) ;
+        Person bob = new Person(VALID_EMAIL, VALID_NAME, VALID_PASSWORD, employeeRole);
+        String encryptedPassword = bob.getEncryptedPassword(VALID_PASSWORD);
+        bob.setPassword(encryptedPassword); // the password save into the system is encrypted
+
+        //Create a game
+        Game game = new Game(VALID_PICTURE, VALID_GAME_NAME, VALID_DESCRIPTION, VALID_PRICE, VALID_QUANTITY, VALID_CATEGORY);
+        
+        when(personRepository.findPersonByEmail(VALID_EMAIL)).thenReturn(bob);
+
+        try {
+            //Act 
+        WishlistLink createdWishlist = wishlistService.addGameToWishlist(VALID_EMAIL, VALID_GAME_NAME);
+        } catch (Exception e) {
+        assertEquals("No customer with this email", e.getMessage());
+        }
+        
+    }
+
+    public void testAlreadyExistingAddGame(){
+         //Create a customer
+         Cart cart = new Cart();
+         Customer customerRole = new Customer(VALID_CARD_NUM, VALID_DATE, VALID_BILLING_ADDRESS, cart);
+         Person bob = new Person(VALID_EMAIL, VALID_NAME, VALID_PASSWORD, customerRole);
+         String encryptedPassword = bob.getEncryptedPassword(VALID_PASSWORD);
+         bob.setPassword(encryptedPassword); // the password save into the system is encrypted
+
+        //Create a game
+        Game game = new Game(VALID_PICTURE, VALID_GAME_NAME, VALID_DESCRIPTION, VALID_PRICE, VALID_QUANTITY, VALID_CATEGORY);
+
+        WishlistLink existingWishlist = new WishlistLink(game, customerRole);
+        
+        when(personRepository.findPersonByEmail(VALID_EMAIL)).thenReturn(bob);
+        when(gameRepository.findGameByName(VALID_GAME_NAME)).thenReturn(game);
+        when(wishlistLinkRepository.save(any(WishlistLink.class))).thenAnswer((InvocationOnMock iom) -> iom.getArgument(0));
+        when(customerRepository.save(any(Customer.class))).thenAnswer((InvocationOnMock iom) -> iom.getArgument(0));
+        when(wishlistLinkRepository.findWishlistLinkByKey(new WishlistLink.Key(game, customerRole))).thenReturn(existingWishlist);
+
+        try {
+        //Act 
+        WishlistLink createdWishlist = wishlistService.addGameToWishlist(VALID_EMAIL, VALID_GAME_NAME);
+        } catch (Exception e) {
+        assertEquals("The Game is already at the customer wishlist", e.getMessage());
+        }
+        
+    }
+
+    @SuppressWarnings("null")
+    @Test
+    public void testRemoveGameWishList(){
+        //Create a game
+        Game game = new Game(VALID_PICTURE, VALID_GAME_NAME, VALID_DESCRIPTION, VALID_PRICE, VALID_QUANTITY, VALID_CATEGORY);
+
+        //Create a cart and customer
+        Cart cart = new Cart();
+        Customer customerRole = new Customer(VALID_CARD_NUM, VALID_DATE, VALID_BILLING_ADDRESS, cart);
+
+        //Create Wishlist
+        WishlistLink existingWishlist = new WishlistLink(game, customerRole);
+
+        //Create a customer
+        customerRole.addCustomerWish(existingWishlist); // the customer has 1 game in his wishlist
+        Person bob = new Person(VALID_EMAIL, VALID_NAME, VALID_PASSWORD, customerRole);
+        String encryptedPassword = bob.getEncryptedPassword(VALID_PASSWORD);
+        bob.setPassword(encryptedPassword); // the password save into the system is encrypted
+
+
+        
+        
+        when(personRepository.findPersonByEmail(VALID_EMAIL)).thenReturn(bob);
+        when(gameRepository.findGameByName(VALID_GAME_NAME)).thenReturn(game);
+        when(wishlistLinkRepository.findWishlistLinkByKey(new WishlistLink.Key(game, customerRole))).thenReturn(existingWishlist);
+        
+        //Act 
+        Boolean deleted = wishlistService.removeGameFromWishlist(VALID_EMAIL, VALID_GAME_NAME);
+
+        //Assert
+        //Verify that a game was added to the customer role
+        assertEquals(deleted, true);
+        verify(wishlistLinkRepository, times(1)).delete(existingWishlist);
+        assertEquals(customerRole.getCustomerWish().size(), 0);
+
+        
+    }
+
+
+
 }
 
