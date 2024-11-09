b7c9f289520d3987ea28f001db7ca68779140b56
Joel Boyer <157649920+joelboyer293@users.noreply.github.com>
2024-11-09 00:34:37 -0500

    Test find order, cart, specific game

--- GameOn-Backend/src/test/java/ca/mcgill/ecse321/GameOn/service/PurchaseGameServiceTests.java
+++ GameOn-Backend/src/test/java/ca/mcgill/ecse321/GameOn/service/PurchaseGameServiceTests.java
@@ -1,6 +1,8 @@
 package ca.mcgill.ecse321.GameOn.service;
 
 
+
+import ca.mcgill.ecse321.GameOn.model.*;
 import ca.mcgill.ecse321.GameOn.repository.CartRepository;
 import ca.mcgill.ecse321.GameOn.repository.OrderRepository;
 import ca.mcgill.ecse321.GameOn.repository.SpecificGameRepository;
@@ -8,6 +10,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Date;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class PurchaseGameServiceTests {
@@ -27,48 +35,177 @@ public class PurchaseGameServiceTests {
 
     //Find Cart by ID
     @Test
-    public void testFindValidCart() {}
+    public void testFindValidCart() {
+        //Arrange
+        int id = 420;
+        Cart aCart = new Cart();
+        when(cartRepository.findCartById(id)).thenReturn(aCart);
+
+        //Act
+        Cart cart = purchaseGameService.findCartByID(id);
+
+        //Assert
+        assertNotNull(cart);
+    }
     @Test
-    public void testFindInvalidCart() {}
+    public void testFindInvalidCart() {
+        //Arrange
+        int id = 420;
+        when(cartRepository.findCartById(id)).thenReturn(null);
+
+        //Act
+        //Assert
+        Exception e = assertThrows(Exception.class, () -> purchaseGameService.findCartByID(id));
+        assertEquals("There are no cart with the ID: " + id + ".", e.getMessage());
+    }
 
     //Find Specific Game by ID
     @Test
-    public void testFindValidSpecificGame() {}
-    @Test public void testFindInvalidSpecificGame() {}
+    public void testFindValidSpecificGame() {
+        //Arrange
+        int id = 420;
+
+        String aPicture = "url";
+        String aName = "Elden Ring";
+        String aDescription = "Open World Dark Souls";
+        int aPrice = 60;
+        int aQuantity = 100;
+        Category aCategory = new Category("Open World");
+
+        Game aGame = new Game(aPicture, aName, aDescription, aPrice, aQuantity, aCategory);
+        SpecificGame aSpecificGame = new SpecificGame(aGame);
+
+        when(specificGameRepository.findSpecificGameById(id)).thenReturn(aSpecificGame);
+
+        //Act
+        SpecificGame specificGame = purchaseGameService.findSpecificGameById(id);
+
+        //Assert
+        assertNotNull(specificGame);
+        assertEquals(aSpecificGame.getGame(), specificGame.getGame());
+    }
+    @Test public void testFindInvalidSpecificGame() {
+        //Arrange
+        int id = 420;
+        when(specificGameRepository.findSpecificGameById(id)).thenReturn(null);
+
+        //Act
+        //Assert
+        Exception e = assertThrows(Exception.class, () -> purchaseGameService.findSpecificGameById(id));
+        assertEquals("There are no specific game with the ID: " + id + ".", e.getMessage());
+
+    }
 
     //Find Order by ID
     @Test
-    public void testFindValidOrder() {}
+    public void testFindValidOrder() {
+        //Arrange
+        int id = 420;
+
+        Date aDate = new Date(System.currentTimeMillis());
+        Cart aCart = new Cart();
+
+        int aCardNum = 21432141;
+        Date aCardExpiryDate = new Date(System.currentTimeMillis());
+        String aBillingAddress = "434 Waterloo Street";
+        Customer aCustomer = new Customer(aCardNum, aCardExpiryDate, aBillingAddress, aCart);
+
+        Order aOrder = new Order(aDate, aCart, aCustomer);
+
+        when(orderRepository.findOrderById(id)).thenReturn(aOrder);
+
+        //Act
+        Order order = purchaseGameService.findOrderById(id);
+
+        //Assert
+        assertNotNull(order);
+        assertEquals(aOrder.getPurchaseDate(), order.getPurchaseDate());
+        assertEquals(aOrder.getCart(), order.getCart());
+        assertEquals(aOrder.getOrderCustomer(), order.getOrderCustomer());
+    }
     @Test
-    public void testFindInvalidOrder() {}
+    public void testFindInvalidOrder() {
+        //Arrange
+        int id = 420;
+        when(orderRepository.findOrderById(id)).thenReturn(null);
+
+        //Act
+        //Assert
+        Exception e = assertThrows(Exception.class, () -> purchaseGameService.findOrderById(id));
+        assertEquals("There are no order with the ID: " + id + ".", e.getMessage());
+    }
 
     //Add specific game to cart
     @Test
-    public void testAddValidSpecificGameToCart() {}
+    public void testAddValidSpecificGameToCart() {
+        //Arrange
+        //Act
+        //Assert
+    }
     @Test
-    public void testAddInvalidSpecificGameToCart() {}
+    public void testAddInvalidSpecificGameToCart() {
+        //Arrange
+        //Act
+        //Assert
+    }
     @Test
-    public void testAddSpecificToInvalidCart() {}
+    public void testAddSpecificToInvalidCart() {
+        //Arrange
+        //Act
+        //Assert
+    }
 
     //remove specific game from cart
     @Test
-    public void testRemoveValidSpecificGameFromCart() {}
+    public void testRemoveValidSpecificGameFromCart() {
+        //Arrange
+        //Act
+        //Assert
+    }
     @Test
-    public void testRemoveInvalidSpecificGameFromCart() {}
+    public void testRemoveInvalidSpecificGameFromCart() {
+        //Arrange
+        //Act
+        //Assert
+    }
     @Test
-    public void testRemoveSpecificFromInvalidCart() {}
+    public void testRemoveSpecificFromInvalidCart() {
+        //Arrange
+        //Act
+        //Assert
+    }
 
     //remove all games from cart
     @Test
-    public void testRemoveAllGamesFromCart() {}
+    public void testRemoveAllGamesFromCart() {
+        //Arrange
+        //Act
+        //Assert
+    }
     @Test
-    public void testRemoveAllGamesFromEmptyCart() {}
+    public void testRemoveAllGamesFromEmptyCart() {
+        //Arrange
+        //Act
+        //Assert
+    }
     @Test
-    public void testRemoveAllGamesFromInvalidCart() {}
+    public void testRemoveAllGamesFromInvalidCart() {
+        //Arrange
+        //Act
+        //Assert
+    }
 
     //create order from cart
     @Test
-    public void testCreateOrderFromCart() {}
+    public void testCreateOrderFromCart() {
+        //Arrange
+        //Act
+        //Assert
+    }
     @Test
-    public void testCreateOrderFromInvalidCart() {}
+    public void testCreateOrderFromInvalidCart() {
+        //Arrange
+        //Act
+        //Assert
+    }
 }
