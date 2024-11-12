ff40467430a57c3433f932b353da74e5096e2aaf
JRfeghaly <josephfeghaly2004@gmail.com>
2024-11-12 17:41:22 -0500

    "started purchase game controller"

---
+++ GameOn-Backend/src/test/java/ca/mcgill/ecse321/GameOn/integration/PurchaseGameIntegrationTests.java
@@ -0,0 +1,10 @@
+package ca.mcgill.ecse321.GameOn.integration;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(OrderAnnotation.class)
+@TestInstance(Lifecycle.PER_CLASS)
+public class PurchaseGameIntegrationTests {
+    
+    
+}
