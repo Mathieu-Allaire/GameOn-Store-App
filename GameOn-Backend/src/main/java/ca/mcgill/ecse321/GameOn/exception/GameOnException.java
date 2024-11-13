03657a6d5114da258ce35ae935e77955ce810722
Joel Boyer <157649920+joelboyer293@users.noreply.github.com>
2024-11-13 15:59:12 -0500

    Exception and Exception handler

--- GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/exception/GameOnException.java
+++ GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/exception/GameOnException.java
@@ -1,4 +1,20 @@
 package ca.mcgill.ecse321.GameOn.exception;
 
-public class GameOnException {
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+
+public class GameOnException extends RuntimeException {
+
+    @NonNull
+    private HttpStatus status;
+
+    public GameOnException(@NonNull HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    @NonNull
+    public HttpStatus getStatus() {
+        return status;
+    }
 }
--- GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/exception/GameOnExceptionHandler.java
+++ GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/exception/GameOnExceptionHandler.java
@@ -1,4 +1,14 @@
 package ca.mcgill.ecse321.GameOn.exception;
 
+import ca.mcgill.ecse321.GameOn.dto.ErrorDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GameOnExceptionHandler {
+    @ExceptionHandler(GameOnException.class)
+    public ResponseEntity<ErrorDto> handleGameOnException(GameOnException e) {
+        return new ResponseEntity<>(new ErrorDto(e.getMessage()), e.getStatus());
+    }
 }
