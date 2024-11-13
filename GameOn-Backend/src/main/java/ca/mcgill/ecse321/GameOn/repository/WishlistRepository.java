e80622b7fa31e51cfa29a952a7047027b69b91a6
Neeshal-Imrit <116143878+Neeshal-Imrit@users.noreply.github.com>
2024-11-12 20:25:20 -0500

    Removed wishlist completely. SInce we are using a direct link instead now.

--- GameOn-Backend/src/main/java/ca/mcgill/ecse321/GameOn/repository/WishlistRepository.java
+++
@@ -1,9 +0,0 @@
-package ca.mcgill.ecse321.GameOn.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ca.mcgill.ecse321.GameOn.model.Wishlist;
-
-public interface WishlistRepository extends CrudRepository<Wishlist, Integer> {
-    Wishlist findWishlistById(int id);
-    
-}
