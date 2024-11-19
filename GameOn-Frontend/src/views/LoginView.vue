031a7402515a3bee8f6c5ef89e064dde84d810bd
Luis Felipe Jarquin Romero <luis.jarquinromero@mail.mcgill.ca>
2024-11-19 11:22:51 -0500

    add log in & register layouts

--- GameOn-Frontend/src/views/LoginView.vue
+++ GameOn-Frontend/src/views/LoginView.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
     <h1>Login</h1>
+    <form @submit.prevent="login">
+      <label for="email">Email</label>
+      <input id="email" v-model="email" type="text" required>
+      <label for="password">Password</label>
+      <input id="password" v-model="password" type="password" required>
+      <button type="submit">Login</button>
+      <label>Don't have an account? <RouterLink to="/register">Register</RouterLink></label>
+    </form>
   </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+}
+label {
+  font-weight: bold;
+}
+input {
+  padding: 0.5em;
+  border-radius: 0.5em;
+  border: 1px solid #ccc;
+}
+</style>
---
+++ GameOn-Frontend/src/views/RegisterView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>Create Account</h1>
+        <form @submit.prevent="register">
+            <label for="email">Email</label>
+            <input id="email" v-model="email" type="text" required>
+            <label for="name">Name</label>
+            <input id="name" v-model="name" type="text" required>
+            <label for="password">Password</label>
+            <input id="password" v-model="password" type="password" required>
+            <label if="passwordConfirmation">Confirm Password</label>
+            <input id="passwordConfirmation" v-model="passwordConfirmation" type="password" required>
+            <label for="cardNumber">Card Number</label>
+            <input id="cardNumber" v-model="cardNumber" type="text" required>
+            <label for="expirationDate">Expiration Date</label>
+            <input id="expirationDate" v-model="expirationDate" type="text" required>
+            <label for="billingAddress">Billing Address</label>
+            <input id="billingAddress" v-model="billingAddress" type="text" required>
+            <div class="form-actions">
+                <button type="submit">Create Account</button>
+                <label> Already have an account? <RouterLink to="/login">Login</RouterLink></label>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+    }
+    label {
+        font-weight: bold;
+    }
+    input {
+        padding: 0.5em;
+        border-radius: 0.5em;
+        border: 1px solid #ccc;
+    }
+</style>
